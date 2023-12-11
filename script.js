e0407a47bef0796f21998756f3de1dae25b7976e
dhiya124 <141201632+dhiya124@users.noreply.github.com>
2023-12-11 15:58:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// console.log("hello")
+// console.log("hai")
+// console.log("marian")
+
+
+ 
+{
+    // let a=10
+    // console.log(a)
+    // a=20
+    // console.log(a)
+}
+
+
+    // const a=10
+    // console.log(a)
+    // a=20
+    // console.log(a)
+
+
+// const cars=["bmw","audi","nissan"]
+// console.log(cars)
+
+// const person={
+//     name:"dhiya",
+//     Age :"25",
+//     place :"kottayam"
+// }
+// console.log(person.name)
+
+// const num1=[1,2,3]
+// const num2=[4,5,6]
+// const num3=[...num1,...num2]
+// console.log(num3)
+
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+// for(var i=0;i<arr.length;i++)
+// {
+//     if(arr[i]%2==0)
+//     {
+//         console.log("even")
+
+//     }
+//     else if(arr[i]%2!=0)
+//     {
+//         console.log("odd")
+//     }
+// }
+
+// var num=2
+// for( var i=1;i<=10;i++)
+// {
+//     console.log(num+"*"+i+"="+num*i)
+// }
+
+var input=5
+var i=0
+while(i<=10){
+    console.log(input+"*"+i+"="+input*i)
+    i++
+}
+
+
+do{
+    console.log(input+"*"+i+"="+input*i)
+    i++
+}while(i<=10)
